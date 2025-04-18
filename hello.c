3421b26e6ecda2b3c066b98316e6e61664f408a6
TongTONg0408 <wu204080@gmail.com>
2025-04-18 18:08:33 +0900

    add some files

--- hello.c
+++ hello.c
@@ -1,7 +1,27 @@
-#include<stdio.h>
+// #include<stdio.h>
+
+#include <stdio.h>
 
 int main(){
-    printf("Hello World!");
+    int a =1;
+    int b = 2;
+    int c = a==b?a:b;
+    if(a==b) {
+        c = a;
+    }else if(b>a){
+        c = b;
+    }else {
+        c = b;
+    }
+    int i = 0;
+    for(;i<10;i++) {
+        printf("%d\n",i);
+    }
+    while(i>0-1) {
+        printf("%d\n",i--);
+    }
+    printf("%%d\"",c);
     return 0;
+    for ()
 }
 
